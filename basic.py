4aa260b2003ef2a075fbb96c7fcbfe9edde6fb71
sunilsala88 <sunilsala88@gmail.com>
2026-05-17 15:17:38 +0530

    may-17-1

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
-api_key=''
-api_secret=''
+api_key='gUwYJAshxoUWwg1YI884pVEO6TmKY6hzeHXS2wv39YVpwAun0h2IkMA96olluF0D'
+api_secret='Raxy9dMGl5a4eXW1kpLw9ztHIwOsavXkFaZJpXHSEjmmy3TyudUCfR9eHPAyFS4L'
 client = Client()
 
 # fetch 1 minute klines for the last day up until now
